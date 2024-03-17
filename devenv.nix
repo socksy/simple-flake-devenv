9bf86b3d5342929d378b030917cfe675628864f4
Ben Lovell <ben.j.lovell@gmail.com>
2024-03-17 17:13:22 +0100

    Add postgres example

--- devenv.nix
+++ devenv.nix
@@ -6,5 +6,23 @@
     figlet -f small -k "Nix shellified!" | lolcat -F 0.5 -ad 1 -s 30
   '';
 
+  # Run an arbitrary program on start
   #processes.run.exec = "hello";
+
+  # Enable programming languages and tooling
+  #languages.python.enable = true;
+  #languages.python.poetry.enable = true;
+  #languages.python.poetry.activate.enable = true;
+  #languages.python.poetry.install.enable = true;
+
+  services.postgres = {
+    enable = true;
+    initialDatabases = [{ name = "foos"; }];
+    listen_addresses = "127.0.0.1";
+    port = 5432;
+    initialScript = ''
+      CREATE USER foo WITH SUPERUSER PASSWORD 'bar';
+      '';
+  };
+  env.PGPASSWORD = "bar";
 }
